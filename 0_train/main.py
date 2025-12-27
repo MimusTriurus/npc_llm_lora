cd431d5be514cf8901629403f890592a596bd9bc
stendhal.syndrome.studio <stendhal.syndrome.studio@gmail.com>
2025-12-27 22:35:55 +0000

    set maximum seq size

--- 0_train/main.py
+++ 0_train/main.py
@@ -3,12 +3,12 @@ import os
 from datasets import load_dataset
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, EarlyStoppingCallback
 from peft import LoraConfig, get_peft_model
-from trl import SFTTrainer, SFTConfig
+from trl import SFTTrainer, SFTConfig, DataCollatorForCompletionOnlyLM
 
 model_name = os.getenv('MODEL_NAME', "Qwen3-4B-Instruct-2507")
 model_path = f'models/{model_name}'
-dataset_file_name = os.getenv('DATASET_FILE_NAME', "training_dataset.jsonl")
-validation_dataset_file_name = os.getenv('VALIDATION_DATASET_FILE_NAME', "validation_dataset.jsonl")
+dataset_file_name = os.getenv('DATASET_FILE_NAME', "4_training_dataset.jsonl")
+validation_dataset_file_name = os.getenv('VALIDATION_DATASET_FILE_NAME', "4_validation_dataset.jsonl")
 print(f'== Model path: {model_path}\n')
 print(f'== Dataset: {dataset_file_name}\n')
 num_train_epoch = int(os.getenv('NUM_TRAIN_EPOCH', 1))
@@ -20,13 +20,19 @@ early_stopping_callback = EarlyStoppingCallback(
 )
 
 tokenizer = AutoTokenizer.from_pretrained(model_path)
-tokenizer.pad_token = tokenizer.eos_token
+
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
+    print(f"pad_token set as eos_token: {tokenizer.eos_token}")
+
+tokenizer.padding_side = "right"
+#tokenizer.pad_token = tokenizer.eos_token
 
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_use_double_quant=True,
     bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16,
+    bnb_4bit_compute_dtype=torch.bfloat16,
 )
 
 model = AutoModelForCausalLM.from_pretrained(
@@ -37,9 +43,12 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 
 lora_config = LoraConfig(
-    r=8,
-    lora_alpha=16,
-    target_modules=["q_proj", "k_proj", "v_proj", "o_proj"],
+    r=32,
+    lora_alpha=64,
+    target_modules=[
+        "q_proj", "k_proj", "v_proj", "o_proj",
+        "gate_proj", "up_proj", "down_proj", # MLP слои - критичны для логики
+    ],
     lora_dropout=0.05,
     bias="none",
     task_type="CAUSAL_LM",
@@ -74,6 +83,23 @@ dataset = dataset.map(
     remove_columns=dataset["train"].column_names,
 )
 
+
+def analyze_token_lengths():
+    lengths = []
+    for example in dataset["train"]:
+        tokens = tokenizer.encode(example["text"], add_special_tokens=False)
+        lengths.append(len(tokens))
+    max_len = max(lengths)
+    return max_len
+
+max_size = analyze_token_lengths()
+max_size = max(max_size, 1024)
+
+print(f'==> SFTConfig. max_len: {max_size}')
+
+response_template = "<|im_start|>assistant\n"
+collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
+
 sft_config = SFTConfig(
     output_dir=f"outputs/{model_name}",
     per_device_train_batch_size=4,
@@ -86,13 +112,13 @@ sft_config = SFTConfig(
     save_strategy="epoch",
     save_total_limit=2,
     fp16=False,
-    bf16=False,
+    bf16=True,
+    dataset_text_field="text",
+    max_seq_length=max_size,
+    packing=False,
     half_precision_backend="no",
     gradient_checkpointing=True,
     optim="paged_adamw_32bit",
-    max_length=1024,
-    packing=False,
-    dataset_text_field="text",
     report_to="none",
     eval_steps=200,
 )
@@ -101,8 +127,8 @@ trainer = SFTTrainer(
     model=model,
     train_dataset=dataset["train"],
     #eval_dataset=dataset["validation"],
+    data_collator=collator,
     args=sft_config,
-    #callbacks=[early_stopping_callback]
 )
 
 trainer.train()
